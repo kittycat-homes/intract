58205aaf8cd7683b086afcab2d849deca948b06b
zoe <ZoeOosting@outlook.com>
2023-07-02 19:32:57 +0200

    e!

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -24,6 +24,7 @@ async fn empty_register() {
         .oneshot(
             Request::builder()
                 .method("POST")
+                .header("Content-Type", "application/json")
                 .uri("/api/v1/account/register")
                 .body(Body::empty())
                 .unwrap(),
@@ -52,6 +53,7 @@ async fn register_owner() -> Response {
         .oneshot(
             Request::builder()
                 .method("POST")
+                .header("Content-Type", "application/json")
                 .uri("/api/v1/account/register")
                 .body(body)
                 .unwrap(),
@@ -71,8 +73,40 @@ async fn register_new_owner() {
 #[tokio::test]
 #[serial]
 async fn try_registering_existing_account() {
-    assert_ne!(register_owner().await.status(), StatusCode::OK)
+    assert_eq!(
+        register_owner().await.status(),
+        StatusCode::INTERNAL_SERVER_ERROR
+    )
 }
 
 #[tokio::test]
-async fn login_unapproved() {}
+#[serial]
+async fn register_no_password() {
+    let body = Body::from(
+        json!(RegisterData {
+            join_reason: None,
+            username: "meow".into(),
+            password: "".into()
+        })
+        .to_string(),
+    );
+    let response = app()
+        .await
+        .oneshot(
+            Request::builder()
+                .method("POST")
+                .uri("/api/v1/account/register")
+                .header("Content-Type", "application/json")
+                .body(body)
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+}
+
+#[tokio::test]
+#[serial]
+async fn login_unapproved() {
+
+}
