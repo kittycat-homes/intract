fb04e26c246701b50610bb63bcb6575289d6a46b
zoe <ZoeOosting@outlook.com>
2023-07-04 11:05:27 +0200

    use the good generator instead...

---
+++ web/src/swagger/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountApi';
+export * from './DocsApi';
+export * from './ServerApi';
---
+++ web/src/swagger/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './LoginData';
+export * from './RegisterData';
+export * from './ServerInfo';
+export * from './Session';
+export * from './SystemTime';
+export * from './User';
