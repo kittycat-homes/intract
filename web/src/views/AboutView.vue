1195d95a2ae0a8c2ad207aa8b170fe1b74b1e53e
zoe <ZoeOosting@outlook.com>
2023-07-05 11:08:27 +0200

    fix about page being reversed

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -7,8 +7,15 @@ fetchServerInfo();
 </script>
 
 <template>
+  <h1>about</h1>
   <div class="about">
     <h1 v-if="loading">loading</h1>
     <h1 v-else>{{ info }}</h1>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  font-size: var(--fs-xl);
+}
+</style>
